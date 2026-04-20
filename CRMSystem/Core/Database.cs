b7656f3b685d2c491d084bd179c1e19c11598ea5
una-07 <esirkepulpan8@gmail.com>
2026-04-20 15:48:51 +0500

    Create Database.cs

---
+++ CRMSystem/Core/Database.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using CRMSystem.Models;
+
+namespace CRMSystem.Core
+{
+    public class Database
+    {
+        public List<Client> Clients = new List<Client>();
+        public List<TaskItem> Tasks = new List<TaskItem>();
+    }
+}
